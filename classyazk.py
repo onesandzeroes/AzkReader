4699c976555dc373bcf01e24c8cc09db2f2525ee
Marius Mather <marius.mather@gmail.com>
2012-05-01 12:33:05 +1000

    Split the settings handling off into a separate module

--- classyazk.py
+++ classyazk.py
@@ -1,73 +1,30 @@
 #! /usr/bin/env python3
 
 import re
-import glob
+import classysettings
 import csv
 
-class YesNoQuestion:
-    yes_no_text = "(Y)es      (N)o"
-    response_dict = {'y': True, 'yes': True, 'n': False, 'no': False}
-    def getAns(self):
-        print(self.yes_no_text)
-        response = input().lower()
-        try:
-            self.answer = self.response_dict[response]
-        except KeyError:
-            print("AzkReader doesn't understand what you typed!\n"
-                  "Please type 'y' or 'n' only\n"
-                  )
-            self.getAns()
-    def __init__(self, message):
-        print(message)
-        self.getAns()
-# Rename 'thingsInID' to 'codeVars', 'indexesInID' to 'codeSlices'
-class allSettings:
-    codeVars = []
-    codeSlices = []
-    def createSlice(self, start, end):
-        start, end = int(start), int(end)
-        return slice(start, end)
-    def readOld(self, filename):
-        setting_csv = csv.reader(open(filename), dialect='excel')
-        for row in setting_csv:
-            self.codeVars.append(row['variable'])
-            index_slice = self.createSlice(row['start'], row['end'])
-            self.codeSlices.append(index_slice)
-
-    found_confs = glob.glob('*.conf')
-
-class newSettings(allSettings):
-    codeIndexes = []
-    def askUntilEmpty(self):
-        entered = input()
-        while entered:
-            self.codeVars.append(entered)
-            entered = input()
-    def getNew(self):
-        print("What should the settings file for this dataset be called?")
-        self.userFilename = input()
-        print("What variables need to be extracted from the ID number for each"
-              "trial? Type them one at a time, then ENTER when you're done"
-              )
-        self.askUntilEmpty()
-    def indexes(self):
-        print("Now type where those values are found in the item number.\n"
-              "If they span multiple digits, type them in the form '2-4'\n"
-              )
-        for var in self.codeVars:
-            print(var)
-            enteredIndex = str(input())
-            if len(enteredIndex) > 1:
-                start = int(enteredIndex.split('-')[0]) - 1
-                end = int(enteredIndex.split('-')[1])
-            else:
-                start = int(enteredIndex) - 1
-                end = start + 1
-            self.codeIndexes.append((start, end))
-        for pair in self.codeIndexes:
-            self.codeSlices.append(self.createSlice(*pair))
-
-           
+def yesOrNo(message):
+    print(message)
+    print("(Y)es    (N)o")
+    userResponse = str(input()).lower()
+    if userResponse in ['y', 'yes']: 
+        respond = True
+    elif userResponse in ['n', 'no']:
+        respond = False
+    else:
+        print("AzkReader doesn't understand what you typed!"
+              "Please type 'y' or 'n' only\n")
+        respond = yesOrNo(message)
+    return respond    
+            
+# Not sure if this is the best way to choose an old or new file?            
+useOld = yesOrNo("Use an existing settings file?")
+if useOld:
+    Settings = classysettings.oldSettings()
+else:
+    Settings = classysettings.newSettings()
+   
 # Change the conf files to have a header, use DictReader to parse them,
 # i.e.
 # def writeSettingsFile(filename, things, indexes):
---
+++ classysettings.py
@@ -0,0 +1,70 @@
+import glob
+import csv
+
+# Rename 'thingsInID' to 'codeVars', 'indexesInID' to 'codeSlices'
+class allSettings:
+    codeVars = []
+    codeSlices = []
+    def createSlice(self, start, end):
+        start, end = int(start), int(end)
+        return slice(start, end)
+
+# Leave out the 'Other' option for the moment, it's not really needed
+class oldSettings(allSettings):
+    found_confs = glob.glob('*.conf')
+    def readOld(self):
+        setting_csv = csv.DictReader(open(self.chosen_file), dialect='excel')
+        for row in setting_csv:
+            self.codeVars.append(row['variable'])
+            index_slice = self.createSlice(row['start'], row['end'])
+            self.codeSlices.append(index_slice)
+    def askWhich(self):
+        for optionNum, filename in enumerate(self.found_confs):
+            print('(' + str(optionNum + 1) + ') ' + filename)
+        fileChoice = int(input)
+        self.chosen_file = self.found_confs[fileChoice - 1]
+    def __init__(self):
+        self.askWhich()
+        self.readOld()
+            
+class newSettings(allSettings):
+    codeIndexes = []
+    def getVars(self):
+        print("What variables need to be extracted from the ID number for each"
+              "trial? Type them one at a time, then ENTER when you're done"
+              )
+        entered = input()
+        while entered:
+            self.codeVars.append(entered)
+            entered = input()
+    def indexes(self):
+        print("Now type where those values are found in the item number.\n"
+              "If they span multiple digits, type them in the form '2-4'\n"
+              )
+        for var in self.codeVars:
+            print(var)
+            enteredIndex = str(input())
+            if len(enteredIndex) > 1:
+                start = int(enteredIndex.split('-')[0]) - 1
+                end = int(enteredIndex.split('-')[1])
+            else:
+                start = int(enteredIndex) - 1
+                end = start + 1
+            self.codeIndexes.append((start, end))
+        for pair in self.codeIndexes:
+            self.codeSlices.append(self.createSlice(*pair))
+    def writeSettings(self):
+        filename = self.userFilename + '.conf'
+        out = open(filename, 'w', newline='')
+        csv_out = csv.writer(out, dialect='excel')
+        csv_out.writerow(['variable', 'start', 'end'])
+        zipped = zip(self.codeVars, self.codeIndexes)
+        for i in zipped:
+            csv_out.writerow([i[0], i[1][0], i[1][1]])
+        out.close()
+    def __init__(self):
+        print("What should the settings file for this dataset be called?")
+        self.userFilename = input()
+        self.getVars()
+        self.indexes()
+        self.writeSettings()
