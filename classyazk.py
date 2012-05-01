b4ad993a82b08cd512f923869ef2711bac1bc494
Marius Mather <marius.mather@gmail.com>
2012-05-01 16:31:03 +1000

    Started adding the file parsing functions

--- classyazk.py
+++ classyazk.py
@@ -25,3 +25,43 @@ if useOld:
 else:
     Settings = classysettings.newSettings()
    
+class AzkFiles:
+    totalSubs_re = re.compile('Subjects\sincorporated')
+    newSub_re = re.compile('Subject\s[0-9]+')
+    trialLine_re = re.compile('\s*[0-9]+\s+-*[0-9\.]+')
+    totalSubs = 0
+    totalMissing = 0
+    allFiles = glob.iglob('*.azk')
+    def __init__(self):
+        for eachFile in allFiles:
+            current = Azk(eachfile)
+
+class Azk(AzkFiles):
+    subID_re = re.compile('ID\s*[0-9a-zA-Z]+')
+    def lookforID(self, line):
+        searched = self.subID_re.search(dmdxLine)
+        if searched:
+            subID = searched.group().split()[1]
+        else: 
+            AzkFiles.totalMissing += 1
+            print('Subject ID missing in ' + self.filename)
+            subID = 'missing' + str(AzkFiles.totalMissing)
+            print('Replaced with ' + subID)
+    def lineType(self, line):
+        line = line.strip()
+        if totalSubs_re.match(line):
+            self.SubsShouldBe = int(line.split(' ')[-1])
+        elif newSub_re.match(line):
+            self.fileSubs += 1
+            self.currentSub = self.lookForID(line)
+            self.currentTrial = 0
+        elif trialLine.match(line):
+            self.currentTrial += 1
+            
+            
+    def __init__(self, filename):
+        self.filename = filename
+        self.fileSubs = 0
+        self.currentTrial = 0
+
+class trialLine
--- classysettings.py
+++ classysettings.py
@@ -68,3 +68,5 @@ class newSettings(allSettings):
         self.getVars()
         self.indexes()
         self.writeSettings()
+        
+# Add some if __name__ = '__main__' tests down here
