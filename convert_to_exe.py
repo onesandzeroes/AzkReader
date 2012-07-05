60b58a60d2935054bbf9ead522ee86a46bd55372
Marius Mather <marius.mather@gmail.com>
2012-07-05 16:09:56 +1000

    Added a downloadable zip file to the repo

--- convert_to_exe.py
+++ convert_to_exe.py
@@ -1,6 +1,18 @@
 import subprocess
+import zipfile
+import glob
+import sys
 
 subprocess.call(['C:/Python32/Scripts/cxfreeze.bat', 'azkreader.py', 
                  '--target-dir',  'CompiledExe'
                  ]
                  )
+
+
+
+with zipfile.ZipFile('Downloads/AzkReader_win.zip', 'w') as zip_out:
+    compiled_files = glob.glob('CompiledExe/*')
+    for each in compiled_files:
+        no_path = each.split('\\')[1]
+        zip_out.write(each, 'AzkReader/' + no_path)
+    #assert zip_out.testzip() == None
